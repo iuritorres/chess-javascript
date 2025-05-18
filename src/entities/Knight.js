c670cb0d0c9e4e745eb87d2edeaf94f156c8c568
iuritorres <iuri.t1000@gmail.com>
2025-05-18 20:11:08 -0300

    feat: finish knight move options

--- src/entities/Knight.js
+++ src/entities/Knight.js
@@ -1,3 +1,5 @@
+import { BOARD_SIZE, COLUMNS } from "../constants/index.js";
+import { getSquarePiece } from "../functions/getSquarePiece.js";
 import { ChessPiece } from "./ChessPiece.js";
 
 export class Knight extends ChessPiece {
@@ -10,7 +12,54 @@ export class Knight extends ChessPiece {
   canMove() {}
 
   getMoveOptions({ currentSquare }) {
+    const currentColumn = currentSquare.id[0];
+    const currentRow = Number(currentSquare.id[1]);
+
     const moveOptions = [];
+
+    const topRow = currentRow < BOARD_SIZE ? currentRow + 1 : "";
+    const bottomRow = currentRow > 1 ? currentRow - 1 : "";
+    const leftColumn = COLUMNS[COLUMNS.indexOf(currentColumn) - 1] ?? "";
+    const rightColumn = COLUMNS[COLUMNS.indexOf(currentColumn) + 1] ?? "";
+
+    const twoColumnsBefore = COLUMNS[COLUMNS.indexOf(currentColumn) - 2] ?? "";
+    const twoColumnsAfter = COLUMNS[COLUMNS.indexOf(currentColumn) + 2] ?? "";
+
+    const topSquares = [
+      leftColumn.concat(currentRow + 2),
+      rightColumn.concat(currentRow + 2),
+    ];
+
+    const bottomSquares = [
+      leftColumn.concat(currentRow - 2),
+      rightColumn.concat(currentRow - 2),
+    ];
+
+    const leftSquares = [
+      twoColumnsBefore.concat(topRow),
+      twoColumnsBefore.concat(bottomRow),
+    ];
+
+    const rightSquares = [
+      twoColumnsAfter.concat(topRow),
+      twoColumnsAfter.concat(bottomRow),
+    ];
+
+    [...topSquares, ...bottomSquares, ...leftSquares, ...rightSquares].forEach(
+      (squareId) => {
+        if (squareId.length !== 2) {
+          return;
+        }
+
+        const piece = getSquarePiece({ squareId });
+
+        if (piece && this.color === piece.getAttribute("color")) {
+          return;
+        }
+
+        moveOptions.push(squareId);
+      }
+    );
 
     return moveOptions;
   }
